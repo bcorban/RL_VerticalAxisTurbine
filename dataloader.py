4860da892ec8e361562cebac4671ff9b7e7ea2ef
bcorban <b.corban@student.isae-supaero.fr>
2023-04-13 11:43:08 +0200

    code for generation of the starting state for RL

--- dataloader.py
+++ dataloader.py
@@ -135,7 +135,30 @@ if __name__=="__main__":
             x_val=np.vstack((x_val,df_merged.values))
             y_val=np.vstack((y_val,df_no_first.values))
 
+    #USED TO GENERATE STARTING STATES FOR RL
+    
+    # Cp_max=np.argmax(unique_Cp_mean)
+    # sub_df = df[df['Cp_mean'] == Cp_max]
+        
+    # sub_df=sub_df.iloc[:1000,:]
 
+    # # create a new dataframe with only the selected columns and without the last row
+    # df_no_last = sub_df[cols_to_keep_in][:-1]
+
+    # #add column containing the 'pitch command'
+    
+    # df_no_last['pitch_increment'] = sub_df['pitch'].diff(periods=-1)[:-1]
+    # df_no_last['pitch_increment'] = -1*df_no_last['pitch_increment'] 
+    # if m==2:
+    #     df_no_last_shift=df_no_last.iloc[tau:].copy()
+    #     df_no_last_0 = df_no_last.iloc[:-tau].copy()
+    #     df_no_last_0= df_no_last_0.drop('pitch_increment', axis=1)
+    #     df_no_last_0=df_no_last_0[cols_to_keep_shift]
+    #     df_merged = pd.concat([df_no_last_shift.reset_index(drop=True),df_no_last_0.reset_index(drop=True)], axis=1)
+    # np.savetxt("./NNet_files/starting_history.npy",np.array(df_merged.values)[:10])
+
+
+    
     T_x_train=torch.tensor(x_train)
     T_x_val=torch.tensor(x_val)
     T_x_test=torch.tensor(x_test)
