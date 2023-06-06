e76e4770760421bd9327718570ef73f3987b77b4
bcorban <baptiste.corban@student.isae-supaero.fr>
2023-06-06 11:46:58 +0200

    fixed circular import in config files

--- Carousel/RL_/envs/__init__.py
+++ Carousel/RL_/envs/__init__.py
@@ -1,3 +1,3 @@
 import sys
-sys.path.append('/Users/baptiste/Desktop/EPFL/RL_VerticalAxisTurbine/RL')
+sys.path.append('/Users/PIVUSER/Desktop/RL_VerticalAxisTurbine/Carousel')
 from RL_.envs.CustomEnv import CustomEnv
